195515ac12c0d27d721c8c1af5743b9fa0ac2c9d
Asif Iqbal <asifiqbal@Asifs-MBP.Dlink>
2022-05-19 06:23:24 +0600

    almust done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 let jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -50,6 +50,20 @@ async function run(){
         const result = await todoList.find().toArray();
         res.send(result);
       })
+      app.get('/todolist/:id' , async(req , res )=>{
+        const id = req.params.id;
+        // console.log(id);
+        const query = {_id: ObjectId(id)};
+        const result = await todoList.findOne(query);
+        res.send(result);
+      })
+      app.delete('/todolist/:id', async(req , res ) =>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const result = await todoList.deleteOne(query);
+      res.send(result)
+      })
+  
 
     }
     
